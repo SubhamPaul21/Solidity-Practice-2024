3a0fe2a8321c80d218d9c3762f364d4da21f1195
SubhamPaul21 <subhampaul12@gmail.com>
2024-02-16 21:57:09 +0530

    added code for reading data from a smart contract deployed in goerli testnet

---
+++ ALCHEMY/access_contract_on_goerli.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const ethers = require('ethers');
+
+const countContractABI = [{ "inputs": [], "name": "count", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "dec", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "get", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "inc", "outputs": [], "stateMutability": "nonpayable", "type": "function" }]
+
+// create Provider
+const provider = new ethers.AlchemyProvider("goerli", process.env.TESTNET_API_KEY_ALCHEMY);
+
+async function main() {
+    const countContract = new ethers.Contract(
+        '0x5F91eCd82b662D645b15Fd7D2e20E5e5701CCB7A',
+        countContractABI,
+        provider,
+    )
+
+    const currentCountValue = await countContract.get();
+
+    console.log(currentCountValue.toString());
+}
+main();
+
